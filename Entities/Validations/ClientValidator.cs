ae170aa95f2b02c2e7bb680a1f0f79669ff1a041
Felipe Muniz <78704793+FelipeMunizz@users.noreply.github.com>
2024-11-23 16:42:17 -0300

    Correção Validator Client

--- Entities/Validations/ClientValidator.cs
+++ Entities/Validations/ClientValidator.cs
@@ -9,7 +9,7 @@ namespace Entities.Validations
         {
             RuleFor(x => x.NAME).NotNull().NotEmpty().MaximumLength(200);
             RuleFor(x => x.CPF).NotNull().NotEmpty().MaximumLength(11);
-            RuleFor(x => x.EMAIL).NotNull().NotEmpty().MaximumLength(200);
+            RuleFor(x => x.EMAIL).NotNull().NotEmpty().MaximumLength(200).EmailAddress();
             RuleFor(x => x.PHONE_NUMBER).NotNull().NotEmpty().MaximumLength(11);
             RuleFor(x => x.CEP).NotNull().NotEmpty().MaximumLength(8);
             RuleFor(x => x.ADDRESS).NotNull().NotEmpty().MaximumLength(300);
